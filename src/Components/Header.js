5485146677117a3a8b15ae1525dcc83a05e82944
taehunlim <dkslsdkdl@naver.com>
2019-11-15 13:16:01 +0900

    location aware Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 import styled from 'styled-components';
 
 
@@ -25,6 +25,9 @@ const Item = styled.li`
   width: 80px;
   height: 50px;
   text-align: center;
+  border-bottom: 3px solid
+    ${props => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -37,18 +40,18 @@ const SLink = styled(Link)`
 
 
 
-export default () => (
+export default withRouter(({ location: { pathname }}) => (
   <Header>
       <List>
-          <Item>
+          <Item current={pathname === "/"}>
               <SLink to="/">Movies</SLink>
           </Item>
-          <Item>
+          <Item current={pathname === "/tv"}>
               <SLink to="/tv">TV</SLink>
           </Item>
-          <Item>
+          <Item current={pathname === "/search"}>
               <SLink to="/search">Search</SLink>
           </Item>
       </List>
   </Header>
-);
+));
