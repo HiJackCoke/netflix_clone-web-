5224f00a948a61ac16b33f98b1285a9f663afbd0
limtae <dkslsdkdl@naver.com>
2020-11-16 10:42:22 +0900

    Detail Page #1

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -42,45 +42,33 @@ const Cover = styled.div`
   border-radius: 5px;
 `;
 
+const Data = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
+const Title = styled.span`
+ font-size: 30px;
+ margin: 20px 10px;
+`;
 
-// const Container = styled.div`
-//   height: calc(100vh - 50px);
-//   width: 100%;
-//   position: relative;
-//   padding: 50px;
-// `;
-//
-// const BackDrop = styled.div`
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   background-image: url(${props => props.bgImage});
-//   background-position: center;
-//   background-size: cover;
-//   filter: blur(3px);
-//   opacity: 0.5;
-//   z-index: 0;
-// `;
-//
-// const Content = styled.div`
-//   display: flex;
-//   width: 100%;
-//   position: relative;
-//   z-index: 1;
-//   height: 100%;
-// `;
-//
-// const Cover = styled.div`
-//   width: 30%;
-//   background-image: url(${props => props.bgImage});
-//   background-position: center;
-//   background-size: cover;
-//   height: 100%;
-//   border-radius: 5px;
-// `;
+const Rating = styled.span`
+  font-size: 15px;
+  margin: 10px;
+`;
+
+const Overview = styled.span`
+  font-size: 15px;
+  margin: 10px;
+`;
+
+const Year = styled.span`
+  margin-bottom: 10px;
+  margin-left: 10px;
+`;
+
+const Imdb = styled.image``;
+
 
 
 
@@ -104,6 +92,24 @@ const DetailPresenter = ({
                             : require("../../assets/empty.png")
                     }
                 />
+
+                <Data>
+                    <Title>{result.original_title}</Title>
+                    <Year>{result.release_date}</Year>
+                    <Rating>
+                        ⭐  {" "} {result.vote_average} {" "} / {" "}10
+                    </Rating>
+
+                    <Overview>
+                        {result.overview}
+                    </Overview>
+
+                
+                </Data>
+
+
+
+
             </Content>
         </Container>
     )
