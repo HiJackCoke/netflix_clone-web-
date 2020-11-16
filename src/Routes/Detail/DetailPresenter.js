4893557c844a701c40e130b259355e32d62072bd
limtae <dkslsdkdl@naver.com>
2020-11-16 12:39:08 +0900

    #1

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 import styled from "styled-components";
 import Loader from "../../Components/Loader";
 
@@ -67,7 +67,7 @@ const Year = styled.span`
   margin-left: 10px;
 `;
 
-const Similar = styled.div``;
+
 
 
 
